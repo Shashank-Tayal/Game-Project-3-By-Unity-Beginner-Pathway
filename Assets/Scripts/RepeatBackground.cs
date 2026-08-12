afc7546ed03866126d066f2151a7b946f124779d
Shashank Tayal <shashanktayal2009@gmail.com>
2026-08-12 20:09:17 +0530

    This make the background move smoothly instead of looking laggy. For that I am using box collider to determine the exact middle value of the background. Also adds comments to the RepeatBackground.cs file for better clarity of the things happening.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    // When the background starts to move we need to repeat it.
+    // Since the background is repeated after half of it is passed, so we will use that point to repeat the background.
+    // Variables:-
     private Vector3 startPosition;
-    public float offset;
+    public float repeatWidth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // With this we can get the start position so that we can reset the background to the starting position.
+        // The box collider will help us with getting the exact half of the background so we will using it for the same.
         startPosition = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // The "/ 2" in the code is for getting the halfway value of the background.
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPosition.x - offset)
+        // Resets the position to the starting position when the background is done halfway through.
+        if (transform.position.x < startPosition.x - repeatWidth)
         {
             transform.position = startPosition;
         }
