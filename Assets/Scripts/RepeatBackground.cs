d1ba121e2436ea1662d4765f386d678a2e6f9b41
Shashank Tayal <shashanktayal2009@gmail.com>
2026-08-12 19:57:09 +0530

    Makes the background repeat itself after certain position. You can change the position of the repetation by changing the value of the offset variable.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPosition;
+    public float offset;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < startPosition.x - offset)
+        {
+            transform.position = startPosition;
+        }
     }
 }
